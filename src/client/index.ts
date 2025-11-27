b0792e1b5edd4709bdb310ac9b66436f6eb07e77
ahm4dd <ahmed.emad.edu@gmail.com>
2025-11-27 20:53:22 +0300

    feat: added actual interactive cli for clients

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,16 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  clientWelcome,
+  commandStatus,
+  getInput,
+  printClientHelp,
+  printQuit,
+} from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind } from "../internal/pubsub/index.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   console.log("Starting Peril client...");
@@ -24,6 +33,43 @@ async function main() {
     PauseKey,
     "transient"
   );
+
+  const gameState = new GameState(name);
+  const input = await getInput();
+
+  while (true) {
+    switch (input[0]) {
+      case "spawn": {
+        console.log(input);
+        commandSpawn(gameState, input);
+        break;
+      }
+      case "move": {
+        const armyMove = commandMove(gameState, input);
+
+        if (armyMove) {
+          console.log("The move was successful");
+        }
+        break;
+      }
+      case "status": {
+        commandStatus(gameState);
+        break;
+      }
+      case "help": {
+        printClientHelp();
+        break;
+      }
+      case "quit": {
+        printQuit();
+        process.exit();
+      }
+      default: {
+        console.log("Command not found");
+        continue;
+      }
+    }
+  }
 }
 
 main().catch((err) => {
